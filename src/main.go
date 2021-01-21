7e920c4b65f215d32e284db0ee6c320e21c00062
Thibault Walterspieler <twalterspieler@protonmail.com>
2021-01-21 22:40:43 +0100

    feat : bootstrap EP

--- src/main.go
+++ src/main.go
@@ -3,40 +3,68 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 func homePage(w http.ResponseWriter, r *http.Request){
-    fmt.Fprintf(w, "Welcome to the HomePage!")
+    fmt.Fprintf(w, "Welcome to the pimento paradise 🌶")
     fmt.Println("Endpoint Hit: homePage")
 }
 
 func handleRequests() {
-	http.HandleFunc("/", homePage)
-	http.HandleFunc("/articles", returnAllArticles)
-    log.Fatal(http.ListenAndServe(":9302", nil))
+    // New instance of a mux router
+    myRouter := mux.NewRouter().StrictSlash(true)
+	myRouter.HandleFunc(apiURL, homePage)
+	myRouter.HandleFunc(fmt.Sprintf("%s/chillies", apiURL), returnAllChillies)
+	myRouter.HandleFunc(fmt.Sprintf("%s/chilli", apiURL), createNewChili).Methods("POST")
+    myRouter.HandleFunc(fmt.Sprintf("%s/chillies/{id}", apiURL), returnSingleChili)
+    log.Fatal(http.ListenAndServe(":667", myRouter))
 }
 
 func main() {
-	Articles = []Article{
-		{Name:"Pimiento", Species:"Capsicum annuum", ScovilleScale:[2]int{100,500}},
-		{Name:"Ghost pepper", Species:"Unknown", ScovilleScale:[2]int{100}},
+	fmt.Println("Go Pimento API v1.0")
+	Chillies = []Chilli{
+		{ID:"1", Name:"Pimiento", Species:"Capsicum annuum", ScovilleScale:[2]int{100,500}},
+		{ID:"2", Name:"Ghost pepper", Species:"Unknown", ScovilleScale:[2]int{100}},
 	}
 	handleRequests()
 }
 
-func returnAllArticles(w http.ResponseWriter, r *http.Request){
+func returnAllChillies(w http.ResponseWriter, r *http.Request){
 	fmt.Println("Endpoint Hit: returnAllArticles")
-	json.NewEncoder(w).Encode(Articles)
+	json.NewEncoder(w).Encode(Chillies)
 }
 
-// Article type 
-type Article struct {
-	Name string `json:"Name"`
-	Species string `json:"Species"`
-	ScovilleScale [2]int `json:"ScovilleScale"`
+func returnSingleChili(w http.ResponseWriter, r *http.Request){
+	vars := mux.Vars(r)
+	key := vars["id"]
+
+for _, chili := range Chillies {
+	if chili.ID == key {
+		fmt.Println("Endpoint Hit: Return", chili.ID)
+		json.NewEncoder(w).Encode(chili)
+	}
+}
 }
 
-// Articles array 
-var Articles []Article;
+func createNewChili(w http.ResponseWriter, r *http.Request) {
+	reqBody, _ := ioutil.ReadAll(r.Body)
+	fmt.Fprintf(w, "%+v", string(reqBody))
+}
+
+const apiURL string = "/api/v1"
+
+// Chilli type 
+type Chilli struct {
+	ID string `json:"id"`
+	Name string `json:"name"`
+	Species string `json:"species"`
+	ScovilleScale [2]int `json:"scoville_scale"`
+}
+
+// Chillies array 
+var Chillies []Chilli;
